798ff119056f2d01b92c42d761e7dbfa4704ce26
PeterWilkens <p.j.wilkens@btinternet.com>
2024-08-07 15:51:06 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,122 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "my-great-book-4",
+  "level": "1",
+  "url": "my-great-book-4.html",
+  "type": "Chapter",
+  "number": "1",
+  "title": "Introduction",
+  "body": "xml:id=\"ch-chapter-title\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Introduction   This Manual describes the C System Chromatic Button Accordion with both Stradella and Free Bass.  The intention is to present information and knowledge, where applicable to the accordion, gained by the author after many years of playing finger-style guitar.  The author was particularly drawn to the button accordion (rather than the piano accordion); noting that Chord and Scale construction on this type of keyboard allow the use of consistent shapes, specific to different Chord and Scale Types, that may be moved anywhere on the keyboard giving a chord identified by the Tonic in that shape.  This use of fingering shapes is similar to the way in which different Chords and Scales are represented in many resources used by Guitar Players, removing the need to read music whilst still being able to learn about Scale and Chord construction and gaining access to the many Chord Progressions. A relatively small number of shapes cover all Scale Chord Types in all inversions.   "
+},
+{
+  "id": "ch-scales-2",
+  "level": "1",
+  "url": "ch-scales-2.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Intervals",
+  "body": "xml:id=\"sec-section-name\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Intervals  Before discussing scales it is useful to understand how intervals between notes are described.  The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval. This interval might be described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting the number of semi-tones on the keyboard or staff but, when counting intervals, the first note, should be counted as one and not zero.   Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    Flat 5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones       A Typical Chromatic Button Accordion    "
+},
+{
+  "id": "ch-scales-2-5",
+  "level": "2",
+  "url": "ch-scales-2.html#ch-scales-2-5",
+  "type": "Table",
+  "number": "2.1.1",
+  "title": "Number Count for the different intervals",
+  "body": " Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    Flat 5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones    "
+},
+{
+  "id": "master-58-b-de-luxe",
+  "level": "2",
+  "url": "ch-scales-2.html#master-58-b-de-luxe",
+  "type": "Figure",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  A Typical Chromatic Button Accordion   "
+},
+{
+  "id": "ch-scales-3",
+  "level": "1",
+  "url": "ch-scales-3.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Major Scale",
+  "body": "xml:id=\"major-scale-col-1\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major Scale  This shape can be moved to any position on the keyboard and the Major scale name is given by the tonic (note 1) of the scale.    Major Scale shape Column 1    "
+},
+{
+  "id": "major-scales-column-1",
+  "level": "2",
+  "url": "ch-scales-3.html#major-scales-column-1",
+  "type": "Figure",
+  "number": "2.2.1",
+  "title": "",
+  "body": "  Major Scale shape Column 1   "
+},
+{
+  "id": "ch-chords-2",
+  "level": "1",
+  "url": "ch-chords-2.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Major Chords",
+  "body": "xml:id=\"major-chords\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major Chords  These shape can be moved to any position on the keyboard and the Major chord name is given by the tonic (note 1) of the chord.    Major 7th Chord Shape Diagram    "
+},
+{
+  "id": "major-chords",
+  "level": "2",
+  "url": "ch-chords-2.html#major-chords",
+  "type": "Figure",
+  "number": "3.1.1",
+  "title": "",
+  "body": "  Major 7th Chord Shape Diagram   "
+},
+{
+  "id": "ch-chords-3",
+  "level": "1",
+  "url": "ch-chords-3.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Major 7th Chords",
+  "body": "xml:id=\"major- 7th-chords2\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major 7th Chords  These shape can be moved to any position on the keyboard and the Major chord name is given by the tonic (note 1) of the chord.    Major 7th Chord Shape Diagram    "
+},
+{
+  "id": "major-7th-chords",
+  "level": "2",
+  "url": "ch-chords-3.html#major-7th-chords",
+  "type": "Figure",
+  "number": "3.2.1",
+  "title": "",
+  "body": "  Major 7th Chord Shape Diagram   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
