1ffb7b45a39bf54ad187f117ea84cfd8580cd3a3
PeterWilkens <p.j.wilkens@btinternet.com>
2024-08-12 16:07:36 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -19,11 +19,29 @@ var ptx_lunr_docs = [
   "body": "xml:id=\"ch-chapter-title\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Authors' Introduction   This Manual describes the C System Chromatic Button Accordion with both Stradella and Free Bass.  The intention is to present information and guidance, where applicable to the accordion, that has influenced me during many years of playing finger-style guitar.  I was particularly drawn to the button accordion (rather than the piano accordion); noting that Chord and Scale construction on this type of keyboard allow the use of consistent shapes, specific to different Chord and Scale Types, that may be moved anywhere on the keyboard giving a chord identified by the Tonic in that shape.  I think there is considerable benefit to the accordion player in considering the \"piano grip systems\" used by jazz pianists and will also include a summary of Mick Goodricks' Voice Leading Almanack's which hold a vast ammount of harmonic information, providing an opportunity for the accordion player to enjoy assimilating the feel of these progressions for themselves. The Almanacks were collated by a jazz guitar player and educator, but they are enormously useful to all players of polyphonic instruments.  This use of fingering shapes is similar to the way in which different Chords and Scales are represented in many resources used by Guitar Players, removing the need to read music whilst still being able to learn about Scale and Chord construction and gaining access to the many Chord Progressions. A relatively small number of shapes cover all Scale Chord Types in all inversions.   "
 },
 {
+  "id": "chromatic-button-accordion",
+  "level": "1",
+  "url": "chromatic-button-accordion.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "The Chromatic Button Accordion",
+  "body": " The Chromatic Button Accordion   Describe how this type of accordion is constructed.     A Typical Chromatic Button Accordion    "
+},
+{
+  "id": "master-58-b-de-luxe",
+  "level": "2",
+  "url": "chromatic-button-accordion.html#master-58-b-de-luxe",
+  "type": "Figure",
+  "number": "2.0.1",
+  "title": "",
+  "body": "  A Typical Chromatic Button Accordion   "
+},
+{
   "id": "ch-scales-intervals-3",
   "level": "1",
   "url": "ch-scales-intervals-3.html",
   "type": "Section",
-  "number": "2.1",
+  "number": "3.1",
   "title": "Scales",
   "body": "xml:id=\"sec-scales\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Scales  Describe how the main scales are constructed. i.e. Major Scale has a sequence of whole tone, whole tone, half tone, whole tone,whole tone, half tone between the tonic note.   Tones between different degrees of a Major Scale.    Scale Degree  Tones between Notes    Tonic to Note 2  Whole Tone    Note 2 to Note 3  Whole Tone    Note 3 to Note 4  Half Tone    Note 4 to Note 5  Whole Tone    Note 5 to Note 6  Whole Tone    Note 6 to Note 7  Whole Tone    Note 7 to Tonic  Half Tone     xml:id=\"Scale-shapes\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Scale Shapes   These shapes can be moved to any position on the keyboard and show the relative position of keyboard buttons for the different Scales.  Diagram of Scales Constructed where Tonic is in Column 1 of the accordion i.e; C, A, E flat or G flat.     Scales from Col 1     "
 },
@@ -32,7 +50,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-scales-intervals-3.html#ch-scales-intervals-3-3",
   "type": "Table",
-  "number": "2.1.1",
+  "number": "3.1.1",
   "title": "Tones between different degrees of a Major Scale.",
   "body": " Tones between different degrees of a Major Scale.    Scale Degree  Tones between Notes    Tonic to Note 2  Whole Tone    Note 2 to Note 3  Whole Tone    Note 3 to Note 4  Half Tone    Note 4 to Note 5  Whole Tone    Note 5 to Note 6  Whole Tone    Note 6 to Note 7  Whole Tone    Note 7 to Tonic  Half Tone    "
 },
@@ -41,7 +59,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-scales-intervals-3.html#scale-col-1",
   "type": "Figure",
-  "number": "2.1.2",
+  "number": "3.1.2",
   "title": "",
   "body": "  Scales from Col 1   "
 },
@@ -50,16 +68,16 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-scales-intervals-4.html",
   "type": "Section",
-  "number": "2.2",
+  "number": "3.2",
   "title": "Intervals",
-  "body": "xml:id=\"sec-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Intervals  Before discussing scales it is useful to understand how intervals between notes are described.  The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval. This interval might be described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting the number of semi-tones on the keyboard or staff but, when counting intervals, the first note, should be counted as one and not zero.   Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    Flat 5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones     xml:id=\"Interval-shapes\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Interval Shapes   These shapes can be moved to any position on the keyboard and show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading and I hope to include some examples of this later in the manual.     Various Interval Shapes       A Typical Chromatic Button Accordion    "
+  "body": "xml:id=\"sec-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Intervals  Before discussing scales it is useful to understand how intervals between notes are described.  The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval. This interval might be described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting the number of semi-tones on the keyboard or staff but, when counting intervals, the first note, should be counted as one and not zero.   Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    Flat 5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones     xml:id=\"Interval-shapes\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Interval Shapes   These shapes can be moved to any position on the keyboard and show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading and I hope to include some examples of this later in the manual.     Various Interval Shapes     "
 },
 {
   "id": "ch-scales-intervals-4-5",
   "level": "2",
   "url": "ch-scales-intervals-4.html#ch-scales-intervals-4-5",
   "type": "Table",
-  "number": "2.2.1",
+  "number": "3.2.1",
   "title": "Number Count for the different intervals",
   "body": " Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    Flat 5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones    "
 },
@@ -68,25 +86,16 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-scales-intervals-4.html#Intervals",
   "type": "Figure",
-  "number": "2.2.2",
+  "number": "3.2.2",
   "title": "",
   "body": "  Various Interval Shapes   "
-},
-{
-  "id": "master-58-b-de-luxe",
-  "level": "2",
-  "url": "ch-scales-intervals-4.html#master-58-b-de-luxe",
-  "type": "Figure",
-  "number": "2.2.3",
-  "title": "",
-  "body": "  A Typical Chromatic Button Accordion   "
 },
 {
   "id": "ch-chords-2",
   "level": "1",
   "url": "ch-chords-2.html",
   "type": "Section",
-  "number": "3.1",
+  "number": "4.1",
   "title": "Major Chords",
   "body": "xml:id=\"major-chords\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major Chords  These shape can be moved to any position on the keyboard and the Major chord name is given by the tonic (note 1) of the chord.    Major 7th Chord Shape Diagram    "
 },
@@ -95,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-chords-2.html#major-chords",
   "type": "Figure",
-  "number": "3.1.1",
+  "number": "4.1.1",
   "title": "",
   "body": "  Major 7th Chord Shape Diagram   "
 },
@@ -104,7 +113,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-chords-3.html",
   "type": "Section",
-  "number": "3.2",
+  "number": "4.2",
   "title": "Major 7th Chords",
   "body": "xml:id=\"major-7th-chords2\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major 7th Chords  These shape can be moved to any position on the keyboard and the Major chord name is given by the tonic (note 1) of the chord.    Major 7th Chord Shape Diagram    "
 },
@@ -113,7 +122,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-chords-3.html#major-7th-chords",
   "type": "Figure",
-  "number": "3.2.1",
+  "number": "4.2.1",
   "title": "",
   "body": "  Major 7th Chord Shape Diagram   "
 },
@@ -122,7 +131,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-chord-progressions-2.html",
   "type": "Section",
-  "number": "4.1",
+  "number": "5.1",
   "title": "Major ii-V-I",
   "body": "xml:id=\"sec-251-progression\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Major ii-V-I  This progression should be practiced in all keys. The most effective method is to use the circle of fifths as discussed in XXXXXXX.    Diagrams of ii V I progressions    "
 },
@@ -131,7 +140,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "ch-chord-progressions-2.html#2-5-1-chord-progression",
   "type": "Figure",
-  "number": "4.1.1",
+  "number": "5.1.1",
   "title": "",
   "body": "  Diagrams of ii V I progressions   "
 },
