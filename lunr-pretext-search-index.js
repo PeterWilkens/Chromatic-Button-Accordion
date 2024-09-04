2ef0bd308392218ef7920b385a05108303f1024d
PeterWilkens <p.j.wilkens@btinternet.com>
2024-09-04 11:57:29 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -70,12 +70,12 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "3.1",
   "title": "Intervals",
-  "body": " Intervals  Before discussing scales it is useful to understand how intervals between notes are described.  The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval. This interval might be described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting the number of semi-tones on the keyboard or staff but, when counting intervals, the first note, should be counted as one and not zero.   Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Diminished 7th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones     xml:id=\"sub-sec-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" 2nd-interval-shapes   It is important to realise that the various shapes can be moved to any position on the keyboard and will maintain the same relationshio between notes in the shape.The shapes show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading.     2nd Interval Shapes     xml:id=\"sub-sec-3rd-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" 3rd-interval-shapes   These shapes can be moved to any position on the keyboard and show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading and I hope to include some examples of this later in the manual.     2nd Interval Shapes     "
+  "body": " Intervals   Before discussing scales it is useful to understand how intervals between notes are described.  The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval. This interval might be described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting the number of semi-tones on the keyboard or staff but, when counting intervals, the first note, should be counted as one and not zero.    Number Count for the different intervals    Interval Type  Count between notes    Minor 2nd  2 Semitones    Major 2nd  3 Semitones    Minor 3rd  4 Semitones    Major 3rd  5 Semitones    Perfect 4th  6 Semitones    5  7 Semitones    5th  8 Semitones    Augmented 5th  9 Semitones    6th  10 Semitones    Diminished 7th  10 Semitones    Minor 7th  11 Semitones    Major 7th  12 Semitones     xml:id=\"sub-sec-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" 2nd-interval-shapes   It is important to realise that the various shapes can be moved to any position on the keyboard and will maintain the same relationshio between notes in the shape.The shapes show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading.     2nd Interval Shapes     xml:id=\"sub-sec-3rd-intervals\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" 3rd-interval-shapes   These shapes can be moved to any position on the keyboard and show the relative position of keyboard buttons for the different intervals.  Using intervals alone it is possible to build the different scales and chords. Knowledge of intervals can also be very useful in sight reading and I hope to include some examples of this later in the manual.     2nd Interval Shapes     "
 },
 {
-  "id": "sec-intervals-5",
+  "id": "sec-intervals-3",
   "level": "2",
-  "url": "sec-intervals.html#sec-intervals-5",
+  "url": "sec-intervals.html#sec-intervals-3",
   "type": "Table",
   "number": "3.1.1",
   "title": "Number Count for the different intervals",
@@ -106,7 +106,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "3.2",
   "title": "Scales",
-  "body": " Scales  All Major Scales have a sequence of whole tone, whole tone, half tone, whole tone,whole tone, half tone between the tonic notes.  An alternative method of generating any major scale would be to count through the Chromatic Scale note to note. Using C Major again as an example, using the following count from the C Tonic note 1 3 5 6 8 10 and 12 will provide the notes of any Major Scale.  Similarly all other scales can be generated using the count specific to that type of scale.  See Section 3.3 on Scale Construction for details of other scales and how they are made up.   Tones between different degrees of a Major Scale.    Scale Degree  Tones between Notes  Type of Interval    Tonic to Note 2  Whole Tone  Major 2nd    Note 2 to Note 3  Whole Tone  Major 2nd    Note 3 to Note 4  Half Tone  Minor 2nd    Note 4 to Note 5  Whole Tone  Major 2nd    Note 5 to Note 6  Whole Tone  Major 2nd    Note 6 to Note 7  Whole Tone  Major 2nd    Note 7 to Tonic  Half Tone  Minor 2nd     xml:id=\"Scale-shapes\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Scale Shapes   It is relatively easy to learn (or build) Scales and Chords on the Chromatic Button Accordion because the Keyboard is laid out in such a fashion that the physical relationship between notes remains constant. Once you know the \"shape\" of a particular scale or chord on the keyboard, that shape can be moved elsewhere on the keyboard and provide chords and scales in all the other Keys.  In other words shapes can be moved to any position on the keyboard and maintain the relative position of keyboard buttons for specific Scale and Chord types.  Diagram of Scales Constructed where Tonic is in Column 1 of the accordion i.e; C, A, E flat or G flat.     Scales from Col 1      Scales from Col 2     "
+  "body": " Scales  All Major Scales have a sequence of whole tone, whole tone, half tone, whole tone,whole tone, half tone between the tonic notes.  An alternative method of generating any major scale would be to count through the Chromatic Scale note to note. Using C Major again as an example, using the following count from the C Tonic note 1 3 5 6 8 10 and 12 will provide the notes of any Major Scale.  Similarly all other scales can be generated using the count specific to that type of scale.  See Section 3.3 on Scale Construction for details of other scales and how they are made up.   Tones between different degrees of a Major Scale.    Scale Degree  Tones between Notes  Type of Interval    Tonic to Note 2  Whole Tone  Major 2nd    Note 2 to Note 3  Whole Tone  Major 2nd    Note 3 to Note 4  Half Tone  Minor 2nd    Note 4 to Note 5  Whole Tone  Major 2nd    Note 5 to Note 6  Whole Tone  Major 2nd    Note 6 to Note 7  Whole Tone  Major 2nd    Note 7 to Tonic  Half Tone  Minor 2nd     xml:id=\"Scale-shapes\" xmlns:xi=\"http:\/\/www.w3.org\/2001\/XInclude\" Scale Shapes   It is relatively easy to learn (or build) Scales and Chords on the Chromatic Button Accordion because the Keyboard is laid out in such a fashion that the physical relationship between notes remains constant. Once you know the \"shape\" of a particular scale or chord on the keyboard, that shape can be moved elsewhere on the keyboard and provide chords and scales in all the other Keys.  In other words shapes can be moved to any position on the keyboard and maintain the relative position of keyboard buttons for specific Scale and Chord types.  Diagram of Scales Constructed where Tonic is in Column 1 of the accordion i.e; C, A, E or G .     Scales from Col 1      Scales from Col 2     "
 },
 {
   "id": "sec-scales-6",
@@ -142,7 +142,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "3.3",
   "title": "Scale Construction",
-  "body": "- Scale Construction  Arranging the chromatic scale in a a cicular fashion can be useful in the construction of both chords and scales.  Take for example the scale of C Major. Looking at the interval pattern for a Major Scale we see it is given by a Major 2nd, a Major 2nd, a minor 2nd, a Major 2nd, a Major 2nd, a Major 2nd, a minor 2nd back to the Tonic. Using these intervals and starting on the Tonic C gives a scale of:  C,D,E,F,G,A,B back to C.  This is the scale of C Major (A scale with no sharp or flat notes i.e all white notes.)  The same approach can be applied to any tonic on the wheel and using the same interval structure will give the notes of that Tonics' Major Scale.  eg; Taking D as the Tonic would give: D, E, F sharp, G, A, B, and C sharp which is the Scale of D Major.      12 tone chromatic scale    The following scores show the intervals between notes in different scale types.    Scales of different types in C for convenience      Scales of different types    "
+  "body": "- Scale Construction  Arranging the chromatic scale in a a cicular fashion can be useful in the construction of both chords and scales.  Take for example the scale of C Major. Looking at the interval pattern for a Major Scale we see it is given by a Major 2nd, a Major 2nd, a minor 2nd, a Major 2nd, a Major 2nd, a Major 2nd, a minor 2nd back to the Tonic. Using these intervals and starting on the Tonic C gives a scale of:  C,D,E,F,G,A,B back to C.  This is the scale of C Major (A scale with no sharp or flat notes i.e all white notes.)  The same approach can be applied to any tonic on the wheel and using the same interval structure will give the notes of that Tonics' Major Scale.  eg; Taking D as the Tonic would give: D, E, F , G, A, B, and C which is the Scale of D Major.      12 tone chromatic scale    The following scores show the intervals between notes in different scale types.    Scales of different types in C for convenience      Scales of different types    "
 },
 {
   "id": "chromatic-scale-disc",
@@ -199,11 +199,20 @@ var ptx_lunr_docs = [
   "body": " Chord Construction Interval Chart    Chord Identification  Interval Structure  Chord Name    Major  1-3-5  T Major    Suspended 2nd  1-2-5  Tsus2    Suspended 4th  1-4-5  Tsus4    Add 9th  1-3-5-9  Tadd9    Sixth  1-3-5-6  T 6    6add9  1-2-5-6-9  T6\/9    Major 7th  1-3-5-7  Tmaj7    Major 9th  1-3-5-7-9  Tmaj9    Major7 11  1-3-5-7- 11  T maj7 11    Major 13th  1-2-5-7-9-13  Tmaj13    Minor  1- 3-5  Tm    minor add9  1- 3-5-9  Tm(add9)    minor 6th  1- 3-5-6  Tm6    minor 6  1- 3-5- 6  Tm 6    minor six add 9  1- 3-5-6-9  Tm6\/9    minor seventh  1- 3-5- 7  Tm7    minor seventh 5  1- 3- 5  Tm7 5    Diminished  1- 3- 5  Tdim    Diminished7  1- 3- 5- 7  Tdim7  >  Augmented  1-3- 5  Taug    minor major seventh  1- 3-5-7-9  Tm(maj7)    minor ninth  1- 3-5- 7-9  Tm9    minor ninth 5  1- 3-5-7-9  Tm9(maj7)    minor ninth 5  1- 3- 5- 7-9  Tm9 5    minor eleventh 5  1- 3-5- 7-9-11  Tm11    minor thirteenth  1- 3-5- 7-9-11-13  Tm13    Dominant 7th  1-3-5- 7  T7    Ninth  1-3-5- 7-9  T9    Eleventh  1-5- 7-9-11  T11    Thirteenth  1-3-5- 7-9-13  T13    "
 },
 {
+  "id": "Triads",
+  "level": "1",
+  "url": "Triads.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Triads and their Inversions",
+  "body": " Triads and their Inversions  Intervals of a Third are at the core of chord construction and Triads are made by stacking the third and fifth note of a scale on top of the Root note. In the C Major Scale, the third interval above C is E and the third interval above E is G. The C Major Triad is thus CEG.  It should also be noted that a Major Triad consists of a Minor Third stacked on top of a Major Third when in the Root position ie; in the case of C Major the Minor Third E to G is stacked on top of the Major Third C to E.  A C Minor Triad is produced in the same fashion by stacking the Third of the C Minor Scale on top of the Root with the Fifth stacked on top of the third, but in the case of the C Minor scale the Third is E and not E, thus the Minor Triad becomes C-E -G demonstrating that a Minor Triad consists of a Major Third Stacked on top of a Minor Third when in the Root Position.  All Triads can be played as Inversions when the order of the notes are changed. eg; 1st Inversion: 3rd, 5th, Root and 2nd Inversion Fifth, Root and 3rd.  The other main Triads are: The Diminished Triad and The Augmented Triad. They are combinations of dissimilar thirds. ie; the Diminished Triad is constructed from a minor third built onto a minor third and an Augmented Triad is constructed from a major third built onto a major third. Diagram 6.1.2 above shows the shape of each Chord. The Tonic note 1 gives the identity ie; if note 1 is D the Triad is a D Chord. Minor, Diminished or Augmented dependant on shape    "
+},
+{
   "id": "sec-7th-chords",
   "level": "1",
   "url": "sec-7th-chords.html",
   "type": "Section",
-  "number": "4.2",
+  "number": "4.3",
   "title": "7th Chords",
   "body": " 7th Chords  These shapes can be moved to any position on the keyboard and the Chord identity is given by the Tonic of the chord (in the following example the Tonic is E)  Note how the chord is changed by altering the seventh note by an interval of a semitone.  In each case the seventh chord is construced by adding a seventh of a particular type to a Triad. In the case of a Major triad adding a Major 7th produces a Major seventh chord, but adding a minor seventh produces a Dominant Seventh chord.    7th Chord Shape Diagrams    Table 4.2.2. below shows how adding different 7th notes to a Triad changes the Chord.  The type of 7th is determined by the interval between the Tonic note of the Chord and the 7th note.   Adding a seventh to a Triad    Triad Type  7th Type  Interval  Chord Generated    Major Triad  Major 7th  12 semitones  Major 7th    Major Triad  Minor 7th  11 semitones  Dominant 7th    Minor Triad  Major 7th  12 semitones  MajorMinor7th    Minor Triad  Minor 7th  11 semitones  Minor 7th    Diminished Triad  Minor 7th  11 semitones  Minor7 5    Diminished Triad  Diminished 7th  10 semitones  Diminished 7th     The 7th chords are Diatonic to the Scale they are generated from. As an example look again at Table 4.1.1 earlier in this Chord Construction section and you can see that the Maj 7th, Minor 7th, the Dominant 7th and the Minor 7 5 are all generated fom the Major scale.  The Diminished 7th Chord is interesting as any of its 4 constituent notes can be the Tonic, e.g in the Edim Chord shown above, the Chord can also be named as Gdim, A dim or C dim.  "
 },
@@ -212,7 +221,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-7th-chords.html#sec-seventh-chords",
   "type": "Figure",
-  "number": "4.2.1",
+  "number": "4.3.1",
   "title": "",
   "body": "  7th Chord Shape Diagrams   "
 },
@@ -221,7 +230,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-7th-chords.html#sec-7th-chords-8",
   "type": "Table",
-  "number": "4.2.2",
+  "number": "4.3.2",
   "title": "Adding a seventh to a Triad",
   "body": " Adding a seventh to a Triad    Triad Type  7th Type  Interval  Chord Generated    Major Triad  Major 7th  12 semitones  Major 7th    Major Triad  Minor 7th  11 semitones  Dominant 7th    Minor Triad  Major 7th  12 semitones  MajorMinor7th    Minor Triad  Minor 7th  11 semitones  Minor 7th    Diminished Triad  Minor 7th  11 semitones  Minor7 5    Diminished Triad  Diminished 7th  10 semitones  Diminished 7th    "
 },
@@ -230,7 +239,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-major-7th-chords.html",
   "type": "Section",
-  "number": "4.3",
+  "number": "4.4",
   "title": "Major 7th Chords",
   "body": " Major 7th Chords  These shapes can be moved to any position on the keyboard and the Major chord name is given by the tonic (note 1) of the chord.    Major 7th Chord Shape Diagram    "
 },
@@ -239,7 +248,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-major-7th-chords.html#major-7th-chords",
   "type": "Figure",
-  "number": "4.3.1",
+  "number": "4.4.1",
   "title": "",
   "body": "  Major 7th Chord Shape Diagram   "
 },
@@ -249,8 +258,8 @@ var ptx_lunr_docs = [
   "url": "sec-251-progression.html",
   "type": "Section",
   "number": "5.1",
-  "title": "Major ii-V-I",
-  "body": " Major ii-V-I  The following shows a common progression that apears in many pieces of music of all types.  The chords shown are all in root position and demonstrate that, adding a further note below the Tonic, in these examples bass notes, changes the identity and type of chord.    Diagrams of ii V I progressions      C Major Circle of 5 progression of Major 7ths     Circle of 5ths from the C Major Scale  This progression of chords all come from the scale of C major and follow each other a 4th interval apart. (The circle of fifths generates the same sequence of chords in the reverse order.) The chords are of different inversions and are chosen to provide a smooth transition, one chord to the next (voice leading). All of the chord are known as drop 2 chords where the note that would normally be second from the top is dropped to the bass position. This new bottom note is not necessarily the Root note of that particular chord. You may also notice that the progression is effectively made up of a series of 2-5-1 chords. Playing through this progression is useful in demonstrating smooth voice leading and introduces a range of different chords which are all transferable to other positions on the keyboard, the same shape producing a different chord.    Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)     Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)     Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)    "
+  "title": "Major ii-V-I with added note in Bass.",
+  "body": " Major ii-V-I with added note in Bass.  The following shows how adding an additional note in the bass changes the chord identity and type. The example shows a common progression that apears in many pieces of music of all types.  The chords shown are all in root position the additional note being added below the original Tonic note..    Diagrams of ii V I progressions      C Major Circle of 5 progression of Major 7ths     Circle of 5ths from the C Major Scale  This progression of chords all come from the scale of C major and follow each other a 4th interval apart. (The circle of fifths generates the same sequence of chords in the reverse order.) The chords are of different inversions and are chosen to provide a smooth transition, one chord to the next (voice leading). All of the chord are known as drop 2 chords where the note that would normally be second from the top is dropped to the bass position. This new bottom note is not necessarily the Root note of that particular chord. You may also notice that the progression is effectively made up of a series of 2-5-1 chords. Playing through this progression is useful in demonstrating smooth voice leading and introduces a range of different chords which are all transferable to other positions on the keyboard, the same shape producing a different chord.    Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)     Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)     Circle of 5th Chords Diatonic to C Major   (Collection of Shapes arranged in 5ths in C Major)    "
 },
 {
   "id": "2-5-1-chord-progression",
